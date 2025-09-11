2dfb3ec5019cf1847d78b305a1e5eae55a174112
Steven Deibert <stevekdeibert@gmail.com>
2025-09-10 17:46:44 -0700

    Create sched.cpp

---
+++ apps/personality/src/sched.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Steve Deibert on 9/10/25.
+//
+
+extern "C" {
+#include <zephyr/kernel.h>
+}
+void personality_sched_tick() {
+	// placeholder for scheduler; call from main if preferred
+}
