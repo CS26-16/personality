e3c18cbab8729bacea936bdccf36136a928a6cfc
Steven Deibert <stevekdeibert@gmail.com>
2025-09-10 17:46:41 -0700

    Create main.cpp

---
+++ apps/personality/src/main.cpp
@@ -0,0 +1,22 @@
+extern "C" {
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+}
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+class Blinker {
+public:
+	void loop() const {
+		while (true) {
+			LOG_INF("Tick (C++)");
+			k_sleep(K_SECONDS(1));
+		}
+	}
+};
+
+int main() {
+	LOG_INF("Hello from Personality app (C++17)!");
+	Blinker b;
+	b.loop();
+	return 0;
+}
