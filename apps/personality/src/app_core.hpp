03b34d2c696119ff6062c689f8a9135ac91e17d3
Steven Deibert <stevekdeibert@gmail.com>
2025-09-19 11:57:44 -0700

    Create app_core.hpp

---
+++ apps/personality/src/app_core.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+
+// simple sensor sample struct #TODO: Fully implement with proper sensor setup
+struct Sample {
+	uint8_t data[8];
+};
+
+// Template entrypoint, inlined for zero overhead on MCU
+template <class Platform>
+[[noreturn]] int run_app(Platform& plat) {
+	plat.init_low_power();
+	plat.log("Personality app starting...");
+
+	while (true) {
+		if (plat.sensor_ready()) {
+			Sample s = plat.read_sample();
+			plat.process(s);
+		}
+		plat.sleep_until_event(); // DEEPSLEEP + interrupt
+	}
+}
